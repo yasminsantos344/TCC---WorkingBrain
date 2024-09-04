04097122550dd85498633a5eea3f3a6639d04418
_y.asntos <yasmin.santos344@etec.sp.gov.br>
2024-09-04 18:35:24 -0300

    Reestruturação do código 0409

--- Aluno/js/CadastroAluno.js
+++ Aluno/js/CadastroAluno.js
@@ -1,26 +1,31 @@
 // Lembrar de fazer verificação de senha
 // API de CEP
+var botao = document.getElementById("CadastroAluno");
 
-function CadastroAluno(){
-    console.log('passou!!!');
-    var DadosAluno = $('#Cadastro').serialize();
-
-    $.ajax({
-        method: 'GET',
-        url: 'ContrAluno.php?Matricula',
-        data: DadosAluno,
-
-        beforeSend: function(){
-            $("#resultado").html("Carregando matrículs...");
-        }
-    })
-
-    .done(function(dadosPHP){
-        $("#resultado").html("Matrícula realizada com sucesso!");
-    })
-
-    .fail(function(){
-        $("#ressultado").html("Falha na matrícula");
-    })
+botao.addEventListener("click", function CadastroAluno (){
+    {
+        console.log('passou!!!');
+        var DadosAluno = $('#Cadastro').serialize();
     
-}
+        $.ajax({
+            method: 'GET',
+            url: 'ContrAluno.php?Matricula',
+            data: DadosAluno,
+    
+            beforeSend: function(){
+                $("#resultado").html("Carregando matrículs...");
+            }
+        })
+    
+        .done(function(dadosPHP){
+            $("#resultado").html("Matrícula realizada com sucesso!");
+        })
+    
+        .fail(function(){
+            $("#ressultado").html("Falha na matrícula");
+        })
+        
+    }
+})
+
+
