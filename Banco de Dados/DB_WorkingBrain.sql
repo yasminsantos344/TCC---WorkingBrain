25e50aea7986008bce2cc2db89ec97924a822228
Yas <yasmin.santos344@etec.sp.gov.br>
2024-11-12 22:18:13 -0300

    Organizando Banco de Dados

--- Banco de Dados/DB_WorkingBrain.sql
+++ Banco de Dados/DB_WorkingBrain.sql
@@ -1,18 +1,19 @@
+/* Criação e uso do Banco de Dados */
 create database DB_WorkingBrain;
 
 drop database DB_WorkingBrain;
 
-drop table TB_Aluno;
-
 use DB_WorkingBrain;
 
+/* Criação das tabelas*/
 create table TB_ADM(
-ID_ADM int (6) auto_increment primary key,
-Nome_ADM varchar (60) not null,
-Email_ADM varchar (40) not null unique,
-Senha_ADM varchar (20) not null,
-Celular_ADM varchar (15) not null,
-Status_ADM char (1) not null
+ID_ADM int auto_increment primary key,
+Nome_ADM varchar (60),
+Nascimento_ADM date,
+Email_ADM varchar (40) unique,
+Senha_ADM varchar (20),
+Celular_ADM varchar (15),
+Status_ADM char (1)
 );
 
 select * from TB_ADM;
@@ -22,9 +23,9 @@ Matricula_Aluno int auto_increment primary key,
 Hora_Matricula_Aluno datetime,
 Nome_Aluno varchar (60),
 Data_Nascimento_Aluno date,
-CPF_Aluno bigint,
+CPF_Aluno varchar (20),
 Email_Aluno varchar (40),
-Senha_Aluno varchar (20),
+Senha_Aluno varchar (200),
 Celular_Aluno varchar (15),
 CEP_Aluno varchar (9),
 Endereco_Aluno varchar (30),
@@ -38,14 +39,76 @@ select * from TB_Aluno;
 
 create table TB_Professor(
 ID_Professor int auto_increment primary key,
-Nome_Professor varchar (60) not null,
-Data_Nascimento_Professor date not null,
-CPF_Professor int unique not null,
-Email_Professor varchar (40) not null,
-Senha_Professor varchar (20) not null,
-Celular_Professor varchar (15) not null,
-CEP_Professor varchar (9) not null,
-Endereco_Numero_Professor varchar (6) not null,
-Endereco_Complemento_Professor varchar (20));
+Nome_Professor varchar (60),
+Data_Nascimento_Professor date,
+CPF_Professor varchar (20),
+Email_Professor varchar (40),
+Senha_Professor varchar (20),
+Celular_Professor varchar (15),
+CEP_Professor varchar (9),
+Endereco_Professor varchar (30),
+Bairro_Professor varchar (20),
+Cidade_Professor varchar (20),
+UF_Professor char (2),
+Endereco_Numero_Professor varchar (6),
+Endereco_Complemento_Professor varchar (20),
+Data_Cadastro datetime);
 
 select * from TB_Professor;
+
+create table TB_Solicitar(
+Nome_Professor varchar (60),
+Data_Nascimento_Professor date,
+CPF_Professor varchar (20),
+Email_Professor varchar (40),
+Senha_Professor varchar (20),
+Celular_Professor varchar (15),
+CEP_Professor varchar (9),
+Endereco_Professor varchar (30),
+Bairro_Professor varchar (20),
+Cidade_Professor varchar (20),
+UF_Professor char (2),
+Endereco_Numero_Professor varchar (6),
+Endereco_Complemento_Professor varchar (20),
+Data_Solicitacao datetime);
+
+select * from TB_Solicitar;
+
+create table TB_Cronograma(
+ID_Cronograma int auto_increment primary key,
+Objetivo_Cronograma varchar (20),
+Dias_Cronograma varchar (10),
+Nivel_Cronograma varchar (20)
+);
+
+select * from TB_Cronograma;
+
+create table TB_Materia(
+ID_Materia int primary key auto_increment,
+Nome_Materia varchar (30));
+
+select * from TB_Materia;
+
+create table TB_SolicitarAula(
+ID_Solicitacao int primary key auto_increment,
+Link_Aula varchar (500),
+Assunto_Aula varchar (60),
+ID_Materia int foreign key );
+
+select * from TB_SolicitarAula;
+
+create table TB_Aula(
+ID_Aula int primary key auto_increment,
+Link_Aula varchar (500),
+Assunto_Aula varchar (60));
+
+select * from TB_Aula;
+
+create table TB_Diretriz(
+ID_Diretriz int auto_increment primary key,
+Diretriz text);
+
+select * from TB_Diretriz;
+
+
+
